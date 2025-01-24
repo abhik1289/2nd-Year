83cf88466965091a797850b3ba0d6d5922c98b9e
Abhik <theabhik2020@gmail.com>
2025-01-24 17:04:49 +0530

    main: Added

--- 4th sem/JAVA LAB/LAB_4(24_1)/Question3.java
+++ 4th sem/JAVA LAB/LAB_4(24_1)/Question3.java
@@ -1,15 +1,40 @@
 import java.util.Scanner;
 
+class Apple {
 
-class Apple{
+    public void show() {
+        System.out.println("This is the Apple show method");
+    }
 
-    
+}
+
+class Banana extends Apple {
+    @Override
+    public void show() {
+        System.out.println("This is the Banana show method");
+    }
+
+}
+
+class Cherry extends Apple {
+    @Override
+    public void show() {
+        System.out.println("This is the Cherry show method");
+    }
+
 }
 
 public class Question3 {
 
     public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
+
+        Apple a1 = new Apple();
+        Banana b1 = new Banana();
+        Cherry c1 = new Cherry();
+
+        a1.show();
+        b1.show();
+        c1.show();
 
     }
 
