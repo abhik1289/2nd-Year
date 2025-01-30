eef1f2005355410e4e3b3f25c19eda818e5a4220
Abhik <theabhik2020@gmail.com>
2025-01-31 01:23:07 +0530

    main: Added

--- 4th sem/JAVA LAB/LBA_3(17_1)/Question4.java
+++ 4th sem/JAVA LAB/LBA_3(17_1)/Question4.java
@@ -11,15 +11,6 @@ class Calculator {
         return a - b - c;
     }
 
-    // Subtract two double values
-    public double subtract(double a, double b) {
-        return a - b;
-    }
-
-    // Subtract an integer from a double
-    public double subtract(double a, int b) {
-        return a - b;
-    }
 
     // Subtract an integer array from a base value
     public int subtract(int base, int[] values) {
@@ -37,8 +28,7 @@ public class Question4 {
         // Using different overloaded methods
         System.out.println("Subtracting two integers: " + calc.subtract(10, 5));
         System.out.println("Subtracting three integers: " + calc.subtract(20, 5, 3));
-        System.out.println("Subtracting two doubles: " + calc.subtract(10.5, 2.2));
-        System.out.println("Subtracting an integer from a double: " + calc.subtract(15.7, 4));
+       
 
         int[] values = { 2, 3, 4 };
         System.out.println("Subtracting an array from a base value: " + calc.subtract(20, values));
