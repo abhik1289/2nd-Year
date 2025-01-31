66ebff827d93ed32d20a6dc65d9ce87a20eeeeca
Abhik <theabhik2020@gmail.com>
2025-01-31 17:04:37 +0530

    main: Added

--- 4th sem/JAVA LAB/lab5(31_1)/Question3.java
+++ 4th sem/JAVA LAB/lab5(31_1)/Question3.java
@@ -1,21 +1,43 @@
+import java.util.Scanner;
 
 interface Income {
-    void earnings();
+    double earnings(double basic);
 
-    void deductions();
+    double deductions(double basic);
 
-    void bonus(){};
+    double bonus(double basic);
 }
-class Manager implements Income{
-    public void earnings() {
-        System.out.println("Manager earnings");
+
+class Manager implements Income {
+    public double earnings(double basic) {
+        return basic + (0.8 * basic) + (0.15 * basic);
     }
-    public void deductions() {
-        System.out.println("Manager deductions");
+
+    public double deductions(double basic) {
+        return basic + (.12 * basic);
     }
-    
+
+    public double bonus(double basic) {
+        throw new UnsupportedOperationException("Not yet implemented");
+    }
 }
+
+class SubStaff extends Manager {
+    public double bonus(double basic) {
+        return (0.5 * basic);
+    }
+}
+
 public class Question3 {
-    public static void main() {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter basic salary: ");
+        double basicSalary = sc.nextDouble();
+        Manager m1 = new Manager();
+        SubStaff s1 = new SubStaff();
+        System.out.println("Manager earnings: " + m1.earnings(basicSalary));
+        System.out.println("Manager deductions: " + m1.deductions(basicSalary));
+        System.out.println("Sub-staff bonus: " + s1.bonus(basicSalary));
+        
     }
 }
