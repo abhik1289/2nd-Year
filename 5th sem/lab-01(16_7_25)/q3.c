c146058ea0040ff2600271c94211295c67fdd5e7
Abhik <theabhik2020@gmail.com>
2025-07-16 10:54:42 +0530

    q4,q4

--- 5th sem/lab-01(16_7_25)/q3.c
+++ 5th sem/lab-01(16_7_25)/q3.c
@@ -1,13 +1,79 @@
 #include <stdio.h>
 
-
-void main(){
+int main() {
+    FILE *fp;
     int n;
+
     printf("Enter how many numbers you want to read from file: ");
-    scanf("%d",&n);
+    scanf("%d", &n);
+
     int arr[n];
-    for(int i=0;i<n;++i){
-        arr[i] = scanf("%d",arr[0]);
+
+
+    fp = fopen("numbers.txt", "r");
+    if (fp == NULL) {
+        printf("Error opening file!\n");
+        return 1;
     }
+
+    for (int i = 0; i < n; i++) {
+        fscanf(fp, "%d", &arr[i]);
+    }
+
+    fclose(fp);
+
+    printf("The content of the array: ");
+    for (int i = 0; i < n; i++) {
+        printf("%d ", arr[i]);
+    }
+    printf("\n");
+
     
+    int duplicateCount = 0;
+
+    for (int i = 0; i < n; i++) {
+        int count = 0;
+
+        for (int j = i + 1; j < n; j++) {
+            if (arr[i] == arr[j]) {
+                count++;
+                break; 
+            }
+        }
+
+    
+        int isCounted = 0;
+        for (int k = 0; k < i; k++) {
+            if (arr[k] == arr[i]) {
+                isCounted = 1;
+                break;
+            }
+        }
+
+        if (count > 0 && !isCounted) {
+            duplicateCount++;
+        }
+    }
+
+    printf("Total number of duplicate values = %d\n", duplicateCount);
+
+    int maxCount = 0, mostFrequent = arr[0];
+
+    for (int i = 0; i < n; i++) {
+        int count = 1;
+        for (int j = i + 1; j < n; j++) {
+            if (arr[i] == arr[j]) {
+                count++;
+            }
+        }
+
+        if (count > maxCount) {
+            maxCount = count;
+            mostFrequent = arr[i];
+        }
+    }
+
+    printf("The most repeating element in the array = %d\n", mostFrequent);
+
+    return 0;
 }
---
+++ 5th sem/lab-01(16_7_25)/q4_extra.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+
+int main(){
+    int arr[6]= {12,3,2,5,6,9};
+    int p=2,q=5;
+
+    
+
+    return 0;
+}
